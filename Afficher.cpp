36d12d9a02182e7b0a45763dc46b8c62de0b0e01
remyarthur <101244151+R2myArthur@users.noreply.github.com>
2022-05-31 15:01:37 +0000

    Ajout de Googletest

--- Afficher.cpp
+++ Afficher.cpp
@@ -95,10 +95,12 @@ void Afficher::deroulement_joueur(Joueur &joueur_qui_joue, Joueur &adversaire){
   this->afficher_plateau_haut_Joueur(joueur_qui_joue,adversaire);
   this->afficher_plateau_bas_Joueur(joueur_qui_joue);
   //std::this_thread::sleep_for(std::chrono::milliseconds(3000));
-do{
-  std::cout<<"press enter pour passer au joueur suivant"<<std::endl;
-  std::cin>>myString;
-}while(myString != "\n");
+  while (1) {
+    if (std::cin.get() == '\n') {
+      std::cout << "Enter key is pressed" << std::endl;
+      break;
+    }
+    }
   
   this->ClearScrollback();
 }
